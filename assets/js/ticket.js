0940b82337bd6e1f1a899ac4f4575a9d2a58e631
Jeremy <renhotsai@gmail.com>
2023-09-20 19:43:00 -0400

    feat:add ticket option

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,19 +1,38 @@
 const headLinerList = [
-    { name: "Headliner Pass + 2 Credits", info1: "1 Headliner Ticket", info2: "2 Credits for The 42", amt: 94 },
-    { name: "Headliner Pass + 6 Credits", info1: "1 Headliner Ticket, with great seats!", info2: "6 Credits for The 42", amt: 144 },
-    { name: "Headliner Pass + 12 Credits", info1: "1 Headliner Ticket, with FANTASTIC seats!", info2: "12 Credits for The 42", amt: 204 }
+    { index: 1, name: "Headliner Pass + 2 Credits", info1: "1 Headliner Ticket", info2: "2 Credits for The 42", amt: 94 },
+    { index: 2, name: "Headliner Pass + 6 Credits", info1: "1 Headliner Ticket, with great seats!", info2: "6 Credits for The 42", amt: 144 },
+    { index: 3, name: "Headliner Pass + 12 Credits", info1: "1 Headliner Ticket, with FANTASTIC seats!", info2: "12 Credits for The 42", amt: 204 }
 ];
 
 const classicPassList = [
-    { name: "Classic 2 Credit Pass", info1: "2 Credits for The 42", amt: 69 },
-    { name: "Classic 4 Credit Pass", info1: "4 Credits for The 42", amt: 99 },
-    { name: "Classic 6 Credit Pass", info1: "6 Credits for The 42", amt: 119 }
+    { index: 4, name: "Classic 2 Credit Pass", info1: "2 Credits for The 42", amt: 69 },
+    { index: 5, name: "Classic 4 Credit Pass", info1: "4 Credits for The 42", amt: 99 },
+    { index: 6, name: "Classic 6 Credit Pass", info1: "6 Credits for The 42", amt: 119 }
 ];
 
-const individualTicketList = [{ name: "Individual Ticket", info1: "Single Tickets Starting At", amt: 39, cent: 50 }];
+const individualTicketList = [
+    { index: 7, name: "Individual Ticket", info1: "Single Tickets Starting At", amt: 39, cent: 50 }
+];
 
 
 const ticketPage = "./view/ticket.html"
+
+const ticketTypes = [
+    { name: headLiner, list: headLinerList },
+    { name: classicPass, list: classicPassList },
+    { name: individualTicket, list: individualTicketList }
+]
+
+
+const changeOption = (listName) => {
+    ticketTypes.forEach(ticketType => {
+        if (ticketType.name === listName) {
+            ticketType.list.forEach(ticket => {
+                document.getElementById("").innerHTML += `<option value="${ticket.index}"></option>`
+            });
+        }
+    });
+}
 
 
 const headLinerTickets = () => {
@@ -26,7 +45,7 @@ const headLinerTickets = () => {
         <p class="ticketContent">${ticket.info1}</p>
         <p class="ticketContent">${ticket.info2}</p>
         <p class="amt"><sup>$</sup>${ticket.amt}<sup>*</sup></p>
-        <a href="`+ticketPage+`" class="btn_buy">Buy</a>
+        <a href="`+ ticketPage + `" class="btn_buy">Buy</a>
         </div>`;
         document.getElementById("headLinerTicket").innerHTML += ticketContainer;
     });
@@ -40,7 +59,7 @@ const classicPasses = () => {
         <p class="ticketName">${ticket.name}</p>
         <p class="ticketContent">${ticket.info1}</p>
         <p class="amt"><sup>$</sup>${ticket.amt}<sup>*</sup></p>
-        <a href="`+ticketPage+`" class="btn_buy">Buy</a>
+        <a href="`+ ticketPage + `" class="btn_buy">Buy</a>
         </div>`;
         document.getElementById("classicPasses").innerHTML += ticketContainer;
     });
@@ -55,7 +74,7 @@ const individualTickets = () => {
         <p class="ticketName">${ticket.name}</p>
         <p class="ticketContent">${ticket.info1}</p>
         <p class="amt"><sup>$</sup>${ticket.amt}<sup style="color:#D80032">${ticket.cent}</sup><sup>*</sup></p>
-        <a href="`+ticketPage+`" class="btn_buy">Buy</a>
+        <a href="`+ ticketPage + `" class="btn_buy">Buy</a>
         </div>`;
         document.getElementById("individualTickets").innerHTML += ticketContainer;
     });
