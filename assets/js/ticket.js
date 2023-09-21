014f5886a32b797a89f1970fbbc0b8248a4f60ca
Jeremy <renhotsai@gmail.com>
2023-09-21 02:24:09 -0400

    fix: ticket.js error

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,43 +1,43 @@
 
-const ticketNameGenerate = () => {
-  
-    const ticket_type = document.getElementById("ticket-type").value; 
-    const ticket_name = document.getElementById("ticket-name").value; 
+// const ticketNameGenerate = () => {
 
-    let outputData = ``;
+//     const ticket_type = document.getElementById("ticket-type").value; 
+//     const ticket_name = document.getElementById("ticket-name").value; 
 
-    if (ticket_type === "individual") {
-        outputData += `<option value="single">Single Ticket ($39.5)</option>`
-    } else if (ticket_type === "classic") {
-        outputData += `<option value="credit-two-pass"> Classic 2 Credit Pass ($69)</option>
-                       <option value="credit-four-pass"> Classic 4 Credit Pass ($99)</option>
-                       <option value="credit-six-pass"> Classic 6 Credit Pass ($119)</option>`
-    } else if (ticket_type === "headliner") {
-        outputData += `<option value="headliner-two-pass">Headliner Pass + 2 Credits ($94)</option>
-                       <option value="headliner-six-pass">Headliner Pass + 6 Credits ($144)</option>
-                       <option value="headliner-tweleve-pass">Headliner Pass + 12 Credits ($204)</option>`
-    } else {
-        console.log("Error: ticket type does not exist!");
-    }
-    document.getElementById("ticket-name").innerHTML = outputData;
-}
+//     let outputData = ``;
+
+//     if (ticket_type === "individual") {
+//         outputData += `<option value="single">Single Ticket ($39.5)</option>`
+//     } else if (ticket_type === "classic") {
+//         outputData += `<option value="credit-two-pass"> Classic 2 Credit Pass ($69)</option>
+//                        <option value="credit-four-pass"> Classic 4 Credit Pass ($99)</option>
+//                        <option value="credit-six-pass"> Classic 6 Credit Pass ($119)</option>`
+//     } else if (ticket_type === "headliner") {
+//         outputData += `<option value="headliner-two-pass">Headliner Pass + 2 Credits ($94)</option>
+//                        <option value="headliner-six-pass">Headliner Pass + 6 Credits ($144)</option>
+//                        <option value="headliner-tweleve-pass">Headliner Pass + 12 Credits ($204)</option>`
+//     } else {
+//         console.log("Error: ticket type does not exist!");
+//     }
+//     document.getElementById("ticket-name").innerHTML = outputData;
+// }
 
 const calTicket = () => {
-    const ticket_quantity = document.getElementById("ticket-quantity").value; 
-    const ticket_credit_info = document.getElementById("ticket-credit-info").value; 
-    const ticket_name = document.getElementById("ticket-name").value; 
-    let ticket_price; 
+    const ticket_quantity = document.getElementById("ticket-quantity").value;
+    const ticket_credit_info = document.getElementById("ticket-credit-info").value;
+    const ticket_name = document.getElementById("ticket-name").value;
+    let ticket_price;
     let outputData = ``;
 
     if (ticket_quantity <= 0) {
         outputData += `<p style="color: red;"> Error: Minimum ticket quantity is 1!</p>`;
     } else if (ticket_credit_info.length !== 6) {
-        outputData += `<p style="color: red;"> Error: Credit card number should be 6 digits!</p>`; 
+        outputData += `<p style="color: red;"> Error: Credit card number should be 6 digits!</p>`;
     } else {
         if (ticket_name === "single") {
-            ticket_price = 39.5; 
+            ticket_price = 39.5;
         } else if (ticket_name === "credit-two-pass") {
-            ticket_price = 69; 
+            ticket_price = 69;
         } else if (ticket_name === "credit-four-pass") {
             ticket_price = 99;
         } else if (ticket_name === "credit-six-pass") {
@@ -45,15 +45,15 @@ const calTicket = () => {
         } else if (ticket_name === "headliner-two-pass") {
             ticket_price = 94;
         } else if (ticket_name === "headliner-six-pass") {
-            ticket_price = 144; 
+            ticket_price = 144;
         } else if (ticket_name === "headliner-tweleve-pass") {
-            ticket_price = 204; 
+            ticket_price = 204;
         }
-    
-        const subtotal = ticket_price * ticket_quantity; 
-        const tax = subtotal * 0.13; 
-        const final_price = subtotal + tax; 
-    
+
+        const subtotal = ticket_price * ticket_quantity;
+        const tax = subtotal * 0.13;
+        const final_price = subtotal + tax;
+
         outputData += `<div style="display: flex; 
                                    flex-direction: column; 
                                    border: 2px solid black; 
@@ -66,7 +66,7 @@ const calTicket = () => {
                         <p> Subtotal: $${subtotal.toFixed(2)}</p>
                         <p> Tax (13%): $${tax.toFixed(2)}</p>
                         <p> Final Price: $${final_price.toFixed(2)}</p>
-                       </div>`    
+                       </div>`
     }
 
     document.getElementById("order-summary").innerHTML = outputData;
@@ -90,11 +90,11 @@ const individualTicketList = [
 ];
 
 
-const headLinerTickets = () =>{
+const headLinerTickets = () => {
 
-document.getElementById("headLinerTicket").innerHTML = "";
-headLinerList.forEach(ticket => {
-    const ticketContainer = `
+    document.getElementById("headLinerTicket").innerHTML = "";
+    headLinerList.forEach(ticket => {
+        const ticketContainer = `
 <div class="ticketInfo">
 <p class="ticketName">${ticket.name}</p>
 <p class="ticketContent">${ticket.info1}</p>
@@ -102,36 +102,36 @@ headLinerList.forEach(ticket => {
 <p class="amt"><sup>$</sup>${ticket.amt}<sup>*</sup></p>
 <a href="#" class="btn_buy">Buy</a>
 </div>`;
-    document.getElementById("headLinerTicket").innerHTML += ticketContainer;
-});
+        document.getElementById("headLinerTicket").innerHTML += ticketContainer;
+    });
 }
 const classicPasses = () => {
 
-document.getElementById("classicPasses").innerHTML = "";
-classicPassList.forEach(ticket => {
-    const ticketContainer = `
+    document.getElementById("classicPasses").innerHTML = "";
+    classicPassList.forEach(ticket => {
+        const ticketContainer = `
 <div class="ticketInfo">
 <p class="ticketName">${ticket.name}</p>
 <p class="ticketContent">${ticket.info1}</p>
 <p class="amt"><sup>$</sup>${ticket.amt}<sup>*</sup></p>
 <a href="#" class="btn_buy">Buy</a>
 </div>`;
-    document.getElementById("classicPasses").innerHTML += ticketContainer;
-});
+        document.getElementById("classicPasses").innerHTML += ticketContainer;
+    });
 }
 
 const individualTickets = () => {
 
-document.getElementById("individualTickets").innerHTML = "";
-individualTicketList.forEach(ticket => {
-    const ticketContainer = `
+    document.getElementById("individualTickets").innerHTML = "";
+    individualTicketList.forEach(ticket => {
+        const ticketContainer = `
 <div class="ticketInfo">
 <p class="ticketContent">${ticket.info1}</p>
 <p class="amt"><sup>$</sup>${ticket.amt}<sup style="color:#D80032">${ticket.cent}</sup><sup>*</sup></p>
 <a href="#" class="btn_buy">Buy</a>
 </div>`;
-    document.getElementById("individualTickets").innerHTML += ticketContainer;
-});
+        document.getElementById("individualTickets").innerHTML += ticketContainer;
+    });
 }
 
 const allTickets = () => {
@@ -148,11 +148,14 @@ const ticketTypes = [
 
 
 const ticketTypeOption = () => {
-    document.getElementById("ticket-type").innerHTM = "";
-    ticketTypes.forEach(ticketType => {
-        document.getElementById("ticket-type").innerHTML += `<option value="${ticketType.id}">${ticketType.name}</option>`
-    });
-    changeOption();
+    if (document.getElementById("ticket-type") !== null) {
+
+        document.getElementById("ticket-type").innerHTM = "";
+        ticketTypes.forEach(ticketType => {
+            document.getElementById("ticket-type").innerHTML += `<option value="${ticketType.id}">${ticketType.name}</option>`
+        });
+        changeOption();
+    }
 }
 
 const changeOption = () => {
@@ -166,3 +169,4 @@ const changeOption = () => {
         }
     });
 }
+window.addEventListener("load", ticketTypeOption);
